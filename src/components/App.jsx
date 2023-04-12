e69405ba751de01a7fc27b646892aed19309eb11
MihajloGerbach <108993799+MihajloGerbach@users.noreply.github.com>
2023-04-12 20:50:05 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+import SharedLayout from './SharedLayout/SharedLayout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Home = lazy(() => import("../pages/Home"));
+const Movies = lazy(() => import("../pages/Movies"));
+const MoviesDetails = lazy(() => import("../pages/MoviesDetails"));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const Cast = lazy(() => import('./Cast/Cast'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path='/movies' element={<Movies/>} />
+          <Route path='/movies/:movieId' element={<MoviesDetails />}>
+            <Route path='reviews' element={<Reviews />}/>
+            <Route path='cast' element={<Cast />}/>
+          </Route>
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
